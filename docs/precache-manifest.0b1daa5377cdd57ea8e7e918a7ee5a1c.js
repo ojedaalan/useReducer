13558693e60c33fd2e101db9351e103e0a392592
Alan Ojeda <alan.ojeda@hotmail.com>
2021-09-27 18:52:28 -0300

    docs agregado

---
+++ docs/precache-manifest.0b1daa5377cdd57ea8e7e918a7ee5a1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55951a57ce05d4a485986a1d8ebca7ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8140ba74076c71dece3",
+    "url": "/static/css/main.cfb3722e.chunk.css"
+  },
+  {
+    "revision": "01c06f112595229994a2",
+    "url": "/static/js/2.05f9dcec.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.05f9dcec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8140ba74076c71dece3",
+    "url": "/static/js/main.f037031a.chunk.js"
+  },
+  {
+    "revision": "46f04600f5238c3977c7",
+    "url": "/static/js/runtime-main.836c258a.js"
+  }
+]);
